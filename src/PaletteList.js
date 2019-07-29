168fe5740ab4af09fb432ea1a3c8428ce019f1c6
Urbano Baz <urbanobaz@Urbanos-iMac.attlocal.net>
2019-07-29 12:16:41 -0400

    Almost Finished MiniPalette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -7,7 +7,7 @@ import { wrap } from "module";
 const styles = {
   root: {
     backgroundColor: "blue",
-    height: "100%",
+    height: "100vh",
     display: "flex",
     alignItems: "flex-start",
     justifyContent: "center"
