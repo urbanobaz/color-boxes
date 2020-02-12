b87fc76ba44a2303d59fb97d2329f0f27e180a4c
Urbano Baz <urbanobaz@Urbanos-iMac.attlocal.net>
2020-02-12 18:04:05 -0500

    Finished css refactoring

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,9 +5,12 @@ import MenuItem from "@material-ui/core/MenuItem";
 import Snackbar from "@material-ui/core/Snackbar";
 import CloseIcon from "@material-ui/icons/Close";
 import IconButton from "@material-ui/core/IconButton";
+import { withStyles } from "@material-ui/styles";
 import Slider from "rc-slider";
+
 import "rc-slider/assets/index.css";
-import "./Navbar.css";
+import styles from './styles/NavbarStyles';
+
 
 class Navbar extends Component {
   constructor(props) {
@@ -27,17 +30,17 @@ class Navbar extends Component {
     this.setState({ open: false });
   }
   render() {
-    const { level, changeLevel, showingAllColors } = this.props;
+    const { level, changeLevel, showingAllColors, classes } = this.props;
     const { format, open } = this.state;
     return (
-      <header className="Navbar">
-        <div className="logo">
+      <header className={classes.Navbar}>
+        <div className={classes.logo}>
           <Link to="/">reactcolorpicker</Link>
         </div>
         {showingAllColors && (
-          <div className="slider-container">
+          <div>
             <span>Level: {level}</span>
-            <div className="slider">
+            <div className={classes.slider}>
               <Slider
                 defaultValue={level}
                 min={100}
@@ -48,7 +51,7 @@ class Navbar extends Component {
             </div>
           </div>
         )}
-        <div className="select-container">
+        <div className={classes.selectContainer}>
           <Select value={format} onChange={this.handleFormatChange}>
             <MenuItem value="hex">HEX - #ffffff</MenuItem>
             <MenuItem value="rgb">rgb - rgb(255, 255, 255)</MenuItem>
@@ -84,4 +87,4 @@ class Navbar extends Component {
   }
 }
 
-export default Navbar;
+export default withStyles(styles)(Navbar);
