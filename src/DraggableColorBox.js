217ae37dba5ebdcb9a34c71e86a2fe7008eed06a
Urbano Baz <urbanobaz@Urbanos-iMac.attlocal.net>
2020-02-19 10:35:45 -0500

    Add palette name form

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -10,13 +10,31 @@ const styles = {
     position: "relative",
     cursor: "pointer",
     marginBottom: "-3.5px",
+  },
+  boxContent: {
+    position: "absolute",
+    width: "100%",
+    left: "0px",
+    bottom: "0px",
+    padding: "10px",
+    color: "black",
+    letterSpacing: "1px",
+    textTransform: "uppercase",
+    fontSize: "12px",
+    display: "flex",
+    justifyContent: "space-between"
   }
 }
 
 function DraggableColorBox(props) {
+  const { classes } = props;
   return (
-    <div className={props.classes.root} style={{backgroundColor: props.color}}>
-      {props.name}
+    <div className={classes.root} style={{backgroundColor: props.color}}>
+      <div className={classes.boxContent}>
+        <span>{props.name}</span>
+        <span>X</span>
+      </div>
+      
     </div>
   )
 }
